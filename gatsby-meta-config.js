a8acd3b3e34d29cf55dd7c38d524e9adf757cdca
Junhee Lee <jhlee910609@gmail.com>
2019-10-12 12:32:59 +0900

    github 주소 수정

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -6,7 +6,7 @@ module.exports = {
   siteUrl: `https://dev-junism.netlify.com`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: `https://github.com/jhlee910609`, // Your GitHub account
+    github: `jhlee910609`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
   },
