f102af6916e4a05d35cdc41d96f5a69135f40aab
Junhee Lee <jhlee910609@gmail.com>
2019-10-12 12:18:21 +0900

    add config and change profile image

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   title: `Bee starter`,
   description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  author: `junism`,
+  introduction: `interested in FE esp ReactJS`,
+  siteUrl: `https://dev-junism.netlify.com`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    github: `https://github.com/jhlee910609`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
   },
